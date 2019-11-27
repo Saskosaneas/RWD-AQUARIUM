f7cc5c878e71d33ae7d30290ddb30009b038dd84
Saskosaneas <56069446+Saskosaneas@users.noreply.github.com>
2019-11-27 15:27:16 +0100

    garry flips

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// GARRY MOVES
+// GARRY MOVES  transform: scaleX(-1);
 $(document).ready(function(e) {
     
     var width = 1389;
@@ -6,21 +6,22 @@ $(document).ready(function(e) {
     function goRight() {
         $(".fish1").animate({
         left: width
-      }, 2000, function() {
+      }, 20000, function() {
          setTimeout(goLeft, 50);
+$(this).css("transform","scaleX(-1)");
       });
     }
+
     function goLeft() {
         $(".fish1").animate({
         left:0
-      }, 2000, function() {
+      }, 20000, function() {
          setTimeout(goRight, 50);
+         $(this).css("transform","scaleX(1)");
       });
     }
-
     setTimeout(goRight, 50);
 });
-
 
 // GARRY SOUNDS //
 var audio1 = $(".not-now-gary")[0];
