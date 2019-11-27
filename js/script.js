992684261e9a615f92c011af4bcc5d7f27162a11
Saskosaneas <56069446+Saskosaneas@users.noreply.github.com>
2019-11-27 14:47:42 +0100

    garry sounds + garry animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-$(document).ready(function(){
-    animateDiv();  
+// GARRY MOVES
+$(document).ready(function(e) {
+    
+    var width = 1389;
+    
+    function goRight() {
+        $(".fish1").animate({
+        left: width
+      }, 2000, function() {
+         setTimeout(goLeft, 50);
+      });
+    }
+    function goLeft() {
+        $(".fish1").animate({
+        left:0
+      }, 2000, function() {
+         setTimeout(goRight, 50);
+      });
+    }
+
+    setTimeout(goRight, 50);
 });
 
 
-// removing the things f
+// GARRY SOUNDS //
+var audio1 = $(".not-now-gary")[0];
+$(".fish1").on("mouseenter" , function() {
+	audio1.play();
+});
+//
+var audio = $(".gary-meow")[0];
+$(".fish1").click(function() {
+    audio.play();
+})
